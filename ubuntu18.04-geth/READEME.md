223b361d6c1040107e2fe52f1758c95751fb1a1e
taigamiura22 <taigamiura22@gmail.com>
2019-06-11 21:59:18 +0900

    eth

--- ubuntu18.04-geth/READEME.md
+++ ubuntu18.04-geth/READEME.md
@@ -1,2 +1,4 @@
 ### set up command
 vagrant up --provision
+
+dataディレクトは共通ディレクトとして設置
---
+++ ubuntu18.04-geth/setup.sh
@@ -0,0 +1,42 @@
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+sudo apt update
+sudo apt install -y docker-ce
+
+echo grep -i docker /etc/group
+sudo gpasswd -a $(whoami) docker
+echo grep -i docker /etc/group
+
+echo docker version
+
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt update
+
+sudo apt install -y ethereum
+geth version
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y build-essential
+sudo apt install -y nodejs
+
+node -v
+npm -v
+
+sudo npm install -g truffle@4.1.15
+truffle version
+
+sudo apt install -y git
+git version
