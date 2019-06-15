56f8a1f2de0d1bf417cc540c49d0d6c3161c70db
TaigaMiura <29893795+ taigamiura22@users.noreply.github.com>
2019-06-15 16:38:46 +0900

    add: docker-compose

--- ubuntu18.04-geth/setup.sh
+++ ubuntu18.04-geth/setup.sh
@@ -22,6 +22,11 @@ echo grep -i docker /etc/group
 
 echo docker version
 
+sudo -i
+curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
+
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt update
 
